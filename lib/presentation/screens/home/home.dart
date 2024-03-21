c7e491e81476789407de0ca2207556b8f4a71e4e
Palejandrachavez <a19490311@itmexicali.edu.mx>
2024-03-20 23:23:54 -0700

    avance practica menu 20/mar/2023

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practica3apmovil/config/menu/menu_items.dart';
+import 'package:practica3apmovil/presentation/screens/home/witdgets/CustomListTitle.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +9,23 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('practica'),
+        title: const Text("Practica menu"),
       ),
+      body: const _HomeView(),
     );
   }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: appMenuItems.length, itemBuilder: _getmenuList);
+  }
+
+  Widget _getmenuList(context, index) => CustomListTitle(
+        item: appMenuItems[index],
+      );
 }
---
+++ lib/presentation/screens/home/witdgets/CustomListTitle.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:practica3apmovil/config/menu/menu_items.dart';
+
+class CustomListTitle extends StatelessWidget {
+  const CustomListTitle({super.key, required this.item});
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(item.icon, color: colors.primary),
+      trailing: const Icon(Icons.arrow_forward_ios_outlined),
+      title: Text(item.title),
+      subtitle: Text(item.subtitle),
+      onTap: () {
+        print('Llamado de la liga a donde ir ${item.link}');
+      },
+    );
+  }
+}
